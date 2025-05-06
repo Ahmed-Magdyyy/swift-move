251fafea52c7750b99ed0209bf1bfb2768fb403c
Ahmed Magdy <76597137+Ahmed-Magdyyy@users.noreply.github.com>
2025-05-06 07:34:56 +0300

    first commit

--- config/database.js
+++ config/database.js
@@ -19,15 +19,11 @@ const mongoose = require("mongoose");
 // module.exports = dbConnection;
 
 const dbConnection = () => {
-  // console.log("====================================");
-     console.log("dbbbbbb");
-  // console.log("====================================");
-
   // Connect to MongoDB and start server
   mongoose
     .connect(process.env.MONGO_URI)
     .then((conn) => {
-      console.log(`Server connected to: ${conn.connection.host}`);
+      console.log(`Server connected to DB: ${conn.connection.host}`);
     })
     .catch((err) => {
       console.error("Failed to connect to MongoDB", err);
